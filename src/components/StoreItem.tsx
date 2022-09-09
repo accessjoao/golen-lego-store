c39627a716fa11a171051c03bde7d1057238ba84
Joao Felipe Silveira <joaofe747@gmail.com>
2022-09-09 15:11:16 -0400

    Working on prices

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { currencyFormat } from "../utilities/currencyFormat";
 
 type StoreItemProps = {
   id: number,
@@ -9,7 +10,24 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, description, imgUrl }: StoreItemProps) {
+
+const quantity = 0;
+
   return (
-    <Card></Card>
+    <Card className="h-100">
+    <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="460px"
+        style={{ objectFit: "fill" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline bb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{currencyFormat(price)}</span>
+        </Card.Title>
+        <div className="mt-auto"></div>
+      </Card.Body>
+      </Card>
   )
 }
