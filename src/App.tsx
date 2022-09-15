e735e022d63612b199a9d10cffd6674724b2581c
Joao Felipe Silveira <joaofe747@gmail.com>
2022-09-15 15:29:08 -0400

    Getting ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { Store }  from "./pages/Store";
 import { About }  from "./pages/About";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import backgroundImage1 from "../public/images/Lego_Background_1.jpg"
 import "./App.css"
 
 function App() {
   return (
-    <div className="bg">
+    <div style={{ backgroundImage: `url(${backgroundImage1})`, height: "100vh", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover", overflow: "auto" }}>
     <ShoppingCartProvider>
     <Navbar />
     <Container className="mb-4">
